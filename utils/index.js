f80923dee8279b29e52340089dc7ee259fd3fab9
sagar <sagar.golhar@neosoftmail.com>
2023-04-05 09:48:02 +0530

    js file changes

--- utils/index.js
+++ utils/index.js
@@ -1,15 +1,28 @@
-function isValid(email) {
-    var validRegex = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
-    switch (email) {
-        case '':
-            return false;
-        default:
-            if (email.match(validRegex)) {
-                return true;
-            }
-            return false;
+const validRegex = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+
+function isValid(input, field) {
+    const rules = field.split('|');
+    for (const rule of rules) {
+      if (!check(rule, input)) {
+        console.log('false')
+        return false;
+      }
     }
+    console.log('true')
+    return true;
+  }
+
+function check(val, input) {
+  switch (val) {
+    case 'required':
+      return input.trim() !== '';
+
+    case 'email':
+      return validRegex.test(input);
+  }
 }
+
+
 const submitBtn = document.getElementById("submit");
 (function () {
     submitBtn.addEventListener('click', (event) => {
