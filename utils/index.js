13092a5f003ec7b80668a11aa673dd855721e59d
sagar <sagar.golhar@neosoftmail.com>
2023-04-06 18:34:46 +0530

    all field error

--- utils/index.js
+++ utils/index.js
@@ -1,32 +1,60 @@
-const validRegex = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+const emailRegex = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+const nameRegex = /^[a-zA-Z]+ [a-zA-Z]+$/;
+const mobileRegex = /^[6-9][0-9]{9}$/;
+const passwordRegex = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,24}$/;
 
-function isValid(input, field) {
-    const rules = field.split('|');
+const da = {
+  name: "sagar golhar",
+  mobile: "ygtgfy",
+  email: "sagargolhar46gmail.com",
+  password: "Sg133155@!"
+};
+const err = {
+  name: "required|name",
+  mobile: "required|mobile",
+  email: "required|email",
+  password: "required|password"
+};
+console.log(isValid(da,err))
+function isValid(data, error) {
+  const errors = {};
+  for (const res in data) {
+    const rules = error[res].split("|");
     for (const rule of rules) {
-      if (!check(rule, input)) {
-        console.log('false')
-        return false;
+      if (!check(rule, data[res])) {
+        if (!errors[res]) {
+          errors[res] = [];
+        }
+        errors[res].push(`${res} validation failed`);
       }
     }
-    console.log('true')
-    return true;
   }
+  return Object.keys(errors).length === 0 ? true : errors;
+}
 
 function check(val, input) {
   switch (val) {
-    case 'required':
-      return input.trim() !== '';
+    case "required":
+      return input.trim() !== "";
 
-    case 'email':
-      return validRegex.test(input);
+    case "name":
+      return nameRegex.test(input);
+
+    case "mobile":
+      return mobileRegex.test(input);
+
+    case "email":
+      return emailRegex.test(input);
+
+    case "password":
+      return passwordRegex.test(input);
   }
 }
 
-
-const submitBtn = document.getElementById("submit");
-(function () {
-    submitBtn.addEventListener('click', (event) => {
-        event.preventDefault();
-    })
-}())
+// const submitBtn = document.getElementById("submit");
+// (function () {
+//     submitBtn.addEventListener('click', (event) => {
+//         event.preventDefault();
+//     })
+// }())
 
